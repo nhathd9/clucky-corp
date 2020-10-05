47093eff43f78fa9f8f1bf0d0248eb904236b2ad
nhat <dangnhath03@gmail.com>
2020-10-04 20:18:02 -0400

    Updates

---
+++ precache-manifest.6819ac117f4c0b802eaea8ae3304cc79.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45362afb423eb4aad8f7bc8363761016",
+    "url": "/clucky-corp/index.html"
+  },
+  {
+    "revision": "541a30322fbfd80605e7",
+    "url": "/clucky-corp/static/css/main.155ef317.chunk.css"
+  },
+  {
+    "revision": "8c1ce781bb176bf18b59",
+    "url": "/clucky-corp/static/js/2.fc0174f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clucky-corp/static/js/2.fc0174f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541a30322fbfd80605e7",
+    "url": "/clucky-corp/static/js/main.a2d24f12.chunk.js"
+  },
+  {
+    "revision": "e5cb563f51df3dedc57d",
+    "url": "/clucky-corp/static/js/runtime-main.8833d8b8.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/clucky-corp/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/clucky-corp/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/clucky-corp/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "ead8a118ec1c4a8c5d02b8ba24f04003",
+    "url": "/clucky-corp/static/media/hero.ead8a118.jpg"
+  },
+  {
+    "revision": "2a9f9955a006982c9e35df099868a870",
+    "url": "/clucky-corp/static/media/iphone.2a9f9955.png"
+  }
+]);
